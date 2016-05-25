9ae255aef6269870729fa38b22ca218dde85a0c0
Simon Garnotel <simon.garnotel@u-picardie.fr>
2016-05-25 16:04:01 +0200

    add builder

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SRC_DIR=$(pwd)
+BUILD_DIR='../build'
+QMAKE=qmake
+
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+$QMAKE $SRC_DIR
+make
+cp -R $SRC_DIR/img $BUILD_DIR/img
+cp -R $SRC_DIR/example $BUILD_DIR/example
+
+
+
